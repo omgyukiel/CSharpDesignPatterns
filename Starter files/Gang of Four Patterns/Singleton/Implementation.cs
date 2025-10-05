8ec4fb88be8a7b3a156c605b2863fdda8d1ca430
KennyLevu <kennylevu1@gmail.com>
2025-10-05 00:35:09 -0700

    implement singleton

--- Starter files/Gang of Four Patterns/Singleton/Implementation.cs
+++ Starter files/Gang of Four Patterns/Singleton/Implementation.cs
@@ -1,4 +1,29 @@
 ﻿namespace Singleton
 {
-  
+    public class Logger
+    {
+        private static Logger? _instance;
+
+        public static Logger Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new Logger();
+                }
+                return _instance;
+            }
+        }
+
+        protected Logger()
+        {
+            // Protected constructor to prevent instantiation from outside
+        }
+
+        public void Log(string message)
+        {
+            Console.WriteLine($"Message to log: {message}");
+        }
+    }
 }
