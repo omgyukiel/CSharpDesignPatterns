bdc762487da6960d303ad85ac510889a74b43962
KennyLevu <kennylevu1@gmail.com>
2025-10-05 20:27:07 -0700

    implement composite

--- Starter files/Gang of Four Patterns/Composite/Implementation.cs
+++ Starter files/Gang of Four Patterns/Composite/Implementation.cs
@@ -1,3 +1,66 @@
 ﻿namespace Composite
 {
+    // component
+    public abstract class FileSystemItem
+    {
+        public string Name { get; set; }
+
+        public abstract long GetSize();
+
+        public FileSystemItem(string name)
+        {
+            Name = name;
+        }
+    }
+
+    // leaf
+
+    public class  File : FileSystemItem
+    {
+        private long _size;
+
+        public File(string name, long size) : base(name)
+        {
+            _size = size;
+        }
+
+        public override long GetSize()
+        {
+            return _size;
+        }
+    }
+
+
+    // composite
+    public class Directory : FileSystemItem
+    {
+        private long _size;
+        private List<FileSystemItem> _fileSystemItems = new List<FileSystemItem>();
+
+        public Directory(string name, long size) : base(name)
+        {
+            _size = size;
+        }
+
+
+        public void Add(FileSystemItem itemToAdd)
+        {
+            _fileSystemItems.Add(itemToAdd);
+        }
+
+        public void Remove(FileSystemItem itemToRemove)
+        {
+            _fileSystemItems.Remove(itemToRemove);
+        }
+
+        public override long GetSize()
+        {
+            var treeSize = _size;
+            foreach (var fileSystemItem in _fileSystemItems)
+            {
+                treeSize += fileSystemItem.GetSize();
+            }
+            return treeSize;
+        }
+    }
 }
--- Starter files/Gang of Four Patterns/Composite/Program.cs
+++ Starter files/Gang of Four Patterns/Composite/Program.cs
@@ -1,1 +1,27 @@
 ﻿Console.Title = "Composite";
+
+
+var root = new Composite.Directory("root", 0);
+var topLevelFile = new Composite.File("toplevel.txt", 100);
+
+var topLevelDirectory1 = new Composite.Directory("topleveldirectory1", 4);
+var topLevelDirectory2 = new Composite.Directory("topleveldirectory2", 4);
+
+
+root.Add(topLevelFile);
+root.Add(topLevelDirectory1);
+root.Add(topLevelDirectory2);
+
+
+var subLevelFile1 = new Composite.File("sublevel1.txt", 200);
+var subLevelFile2 = new Composite.File("sublevel2.txt", 300);
+
+topLevelDirectory1.Add(subLevelFile1);
+topLevelDirectory1.Add(subLevelFile2);
+
+
+Console.WriteLine($"Size of {root.Name} is {root.GetSize()} bytes.");
+Console.WriteLine($"Size of {topLevelDirectory1.Name} is {topLevelDirectory1.GetSize()} bytes.");
+Console.WriteLine($"Size of {topLevelDirectory2.Name} is {topLevelDirectory2.GetSize()} bytes.");
+
+Console.ReadKey();
