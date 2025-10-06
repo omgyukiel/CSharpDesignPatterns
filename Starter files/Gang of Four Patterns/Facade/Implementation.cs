8954b8314b9a05c1d8db92b8cd123f11c312c8d4
KennyLevu <kennylevu1@gmail.com>
2025-10-05 18:42:49 -0700

    implement facade and demo

--- Starter files/Gang of Four Patterns/Facade/Implementation.cs
+++ Starter files/Gang of Four Patterns/Facade/Implementation.cs
@@ -1,3 +1,61 @@
 ﻿namespace Facade
 {
+    /// subsystem class
+    /// 
+    public class OrderService
+    {
+        public bool HasEnoughOrders(int customerId)
+        {
+            // dos cust have enough orders?
+            /// fake calculatiionf or dmeo
+            /// 
+            return (customerId > 5);
+        }
+
+    }
+
+    // subsytem class
+
+    public class  CustomerDiscountBaseService
+    {
+        public double CalculateDiscountBase(int customerId)
+        {
+            return (customerId > 8) ? 10 : 20;
+        }
+    }
+
+    // subsystem class
+    public class DayOfTheWeekFactorService
+    {
+        public double CalculateDayOfTheWeekFactor()
+        {
+            switch (DateTime.UtcNow.DayOfWeek)
+            {
+                case DayOfWeek.Saturday:
+                case DayOfWeek.Sunday:
+                    return 0.8;
+                default:
+                    return 1.2;
+            }
+        }
+    }
+
+    /// facade
+    /// 
+    public class DiscountFacade
+    {
+        private readonly OrderService _orderService = new();
+        private readonly CustomerDiscountBaseService _customerDiscountBaseService = new();
+        private readonly DayOfTheWeekFactorService _dayOfTheWeekFactorService = new();
+
+        public double CalculateDiscountPercentage(int customerId)
+        {
+            if (!_orderService.HasEnoughOrders(customerId))
+            {
+                return 0;
+            }
+           return _customerDiscountBaseService.CalculateDiscountBase(customerId) *
+                _dayOfTheWeekFactorService.CalculateDayOfTheWeekFactor();
+        }
+    }
 }
--- Starter files/Gang of Four Patterns/Facade/Program.cs
+++ Starter files/Gang of Four Patterns/Facade/Program.cs
@@ -1,1 +1,10 @@
-﻿Console.Title = "Facade";
+﻿using Facade;
+
+Console.Title = "Facade";
+
+var facade = new DiscountFacade();
+Console.WriteLine($"Discount for customer 3 is {facade.CalculateDiscountPercentage(3)}");
+
+Console.WriteLine($"Discount for customer 10 is {facade.CalculateDiscountPercentage(10)}");
+
+Console.ReadKey();
