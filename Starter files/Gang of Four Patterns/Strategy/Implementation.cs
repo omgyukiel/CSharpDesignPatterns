cbcab08aba2b9e6eb4d4944395ebe15b5a18f57b
KennyLevu <kennylevu1@gmail.com>
2025-10-05 21:13:00 -0700

    implement strategy

--- Starter files/Gang of Four Patterns/Strategy/Implementation.cs
+++ Starter files/Gang of Four Patterns/Strategy/Implementation.cs
@@ -1,3 +1,70 @@
 ﻿namespace Strategy
 {
+
+    // strat
+
+    public interface IExportService
+    {
+        void Export(Order order);
+    }
+
+
+    // concrete start
+
+    public class JsonExportService : IExportService
+    {
+        public void Export(Order order)
+        {
+            Console.WriteLine($"Exporting {order.Name} as JSON");
+        }
+    }
+    // concrete start
+
+    public class XMLExportService : IExportService
+    {
+        public void Export(Order order)
+        {
+            Console.WriteLine($"Exporting {order.Name} as XML.");
+        }
+    }
+
+    // concrete start
+    public class CSVExportService : IExportService
+    {
+        public void Export(Order order)
+        {
+            Console.WriteLine($"Exporting {order.Name} as CSV.");
+        }
+    }
+
+    // concrete mine
+    public class PNGExportService : IExportService
+    {
+        public void Export(Order order)
+        {
+            Console.WriteLine($"Exporting {order.Name} as PNG.");
+        }
+    }
+
+    // context
+    public class Order
+    {
+        public string Customer { get; set; }
+        public int Amount { get; set; }
+        public string Name { get; set; }
+
+        public IExportService? ExportService { get; set; }
+        public Order(string customer, int amount, string name, IExportService? exportService = null)
+        {
+            Customer = customer;
+            Amount = amount;
+            Name = name;
+            ExportService = exportService ?? new CSVExportService();
+        }
+
+        public void Export()
+        {
+            ExportService?.Export(this);
+        }
+    }
 }
--- Starter files/Gang of Four Patterns/Strategy/Program.cs
+++ Starter files/Gang of Four Patterns/Strategy/Program.cs
@@ -1,1 +1,15 @@
-﻿Console.Title = "Strategy";
+﻿using Strategy;
+
+Console.Title = "Strategy";
+
+var order = new Order("Marvin Software", 5, "Visual Studio License");
+order.ExportService = new CSVExportService();
+order.Export();
+order.ExportService = new JsonExportService();
+order.Export();
+order.ExportService = new XMLExportService();
+order.Export();
+order.ExportService = new PNGExportService();
+order.Export();
+
+Console.ReadKey();
