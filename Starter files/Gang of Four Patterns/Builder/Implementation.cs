90bf310ea22b9a8a4f95dffe6934b39d0d6f6823
KennyLevu <kennylevu1@gmail.com>
2025-10-05 18:29:37 -0700

    implement builder pattern with demo

--- Starter files/Gang of Four Patterns/Builder/Implementation.cs
+++ Starter files/Gang of Four Patterns/Builder/Implementation.cs
@@ -1,4 +1,114 @@
-﻿namespace BuilderPattern
-{
+﻿using System.Text;
 
+namespace BuilderPattern
+{
+    /// Product
+    /// 
+    public class Car
+    {
+        private readonly List<string> _parts = new();
+        private readonly string _carType;
+
+        public Car(string carType)
+        {
+            _carType = carType;
+        }
+
+        public void AddPart(string part)
+        {
+            _parts.Add(part);
+        }
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            foreach (string part in _parts)
+            {
+                sb.Append($"Car Type: {_carType}, Part: {part}");
+            }
+
+            return sb.ToString();
+        }
+    }
+
+    /// Builder
+    /// 
+    public abstract class CarBuilder
+    {
+        public Car Car { get; private set; }
+
+        public CarBuilder(string carType)
+        {
+            Car = new Car(carType);
+        }
+
+        public abstract void BuildEngine();
+        public abstract void BuildFrame();
+    }
+
+    /// <summary>
+    /// ConcreateBuilder1 class
+    /// </summary>
+    ///
+    public class MiniBuilder : CarBuilder
+    {
+        public MiniBuilder() : base("Mini")
+        {
+        }
+
+        public override void BuildEngine()
+        {
+            Car.AddPart("'not a v8'");
+        }
+
+        public override void BuildFrame()
+        {
+            Car.AddPart("'3 doors with stripes'");
+        }
+    }
+
+    // <summary>
+    /// ConcreateBuilder2 class
+    /// </summary>
+    ///
+    public class MustangBuilder: CarBuilder
+    {
+        public MustangBuilder() : base("Mustang")
+        {
+        }
+
+        public override void BuildEngine()
+        {
+            Car.AddPart("'a v8'");
+        }
+
+        public override void BuildFrame()
+        {
+            Car.AddPart("'4 doors with flames'");
+        }
+    }
+
+
+    /// Director
+    /// 
+    public class Garage
+    {
+        private CarBuilder? _builder;
+
+        public Garage()
+        {
+        }
+
+        public void Construct(CarBuilder builder)
+        {
+            _builder = builder;
+            _builder.BuildEngine();
+            _builder.BuildFrame();
+        }
+
+        public void Show()
+        {
+            Console.WriteLine(_builder?.Car.ToString());
+        }
+    }
 }
--- Starter files/Gang of Four Patterns/Builder/Program.cs
+++ Starter files/Gang of Four Patterns/Builder/Program.cs
@@ -1,1 +1,19 @@
-﻿Console.Title = "Builder";
+﻿using BuilderPattern;
+
+Console.Title = "Builder";
+
+var garage = new Garage();
+
+var miniBuilder = new MiniBuilder();
+var mustangBuilder = new MustangBuilder();
+
+garage.Construct(miniBuilder);
+Console.WriteLine(miniBuilder.Car.ToString());
+
+garage.Show();
+
+garage.Construct(mustangBuilder);
+Console.WriteLine(mustangBuilder.Car.ToString());
+garage.Show();
+
+Console.ReadKey();
