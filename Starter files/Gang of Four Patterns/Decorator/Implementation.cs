c0c2096c84ca3e98487564a3ff53ddd240b2e660
KennyLevu <kennylevu1@gmail.com>
2025-10-05 19:09:01 -0700

    implement and demo decorator patttern

--- Starter files/Gang of Four Patterns/Decorator/Implementation.cs
+++ Starter files/Gang of Four Patterns/Decorator/Implementation.cs
@@ -1,3 +1,104 @@
 ﻿namespace Decorator
 {
+    /// comoponent
+    /// 
+    public interface IMailService
+    {
+        bool SendMail(string message);
+    }
+
+    // concrete component
+
+    public class  CloudMailService: IMailService
+    {
+        public bool SendMail(string message)
+        {
+            Console.WriteLine($"Message \"{message}\" " + $"sent via {nameof(CloudMailService)}");
+            return true;
+        }
+        
+    }
+
+    // concrete 2
+    public class OnPremiseMailService : IMailService
+    {
+        public bool SendMail(string message)
+        {
+            Console.WriteLine($"Message \"{message}\" " + $"sent via {nameof(OnPremiseMailService)}");
+            return true;
+        }
+
+    }
+    // deocrator
+    public abstract class MailServiceDecoratorBase : IMailService
+    {
+        private readonly IMailService _mailService;
+
+        public MailServiceDecoratorBase(IMailService mailService)
+        {
+            _mailService = mailService;
+        }
+
+        public virtual bool SendMail(string message)
+        {
+            return _mailService.SendMail(message);
+        }
+
+    }
+
+    // concrete decorator
+    public class  StatisticsDecorator : MailServiceDecoratorBase
+    {
+        public StatisticsDecorator(IMailService mailService) : base(mailService)
+        {
+        }
+
+        public override bool SendMail(string message)
+        {
+            // fake collect stats
+            Console.WriteLine($"collecting statistic in {nameof(StatisticsDecorator)}");
+            return base.SendMail(message);
+        }
+    }
+
+
+    // concrete decorator
+    public class MessageDatabaseDecorator : MailServiceDecoratorBase
+    {
+        /// <summary>
+        ///  a list of sent message - a "fake" database
+        /// </summary>
+
+        public List<string> SentMessages { get; private set; } = new List<string>();
+
+        public MessageDatabaseDecorator(IMailService mailService) : base(mailService)
+        { }
+
+        public override bool SendMail(string message)
+        {
+            if (base.SendMail(message))
+            {
+                SentMessages.Add(message);
+                return true;
+            };
+            return false;
+        }
+
+    }
+
+    // my concrete decorator
+    public class AttachCatPngDecorator : MailServiceDecoratorBase
+    {
+        private readonly string _catPngPath;
+        public AttachCatPngDecorator(IMailService mailService, string catPngPath) : base(mailService)
+        {
+                       _catPngPath = catPngPath;
+        }
+
+        public override bool SendMail(string message)
+        {
+           message += $" with cat png from {_catPngPath}";
+           return base.SendMail(message);
+        }
+    }
 }
--- Starter files/Gang of Four Patterns/Decorator/Program.cs
+++ Starter files/Gang of Four Patterns/Decorator/Program.cs
@@ -1,1 +1,27 @@
-﻿Console.Title = "Decorator";
+﻿using Decorator;
+Console.Title = "Decorator";
+
+var cloudMailService = new CloudMailService();
+cloudMailService.SendMail("Hi there.");
+
+var onPremiseMailService = new OnPremiseMailService();
+onPremiseMailService.SendMail("Hello there.");
+
+// add behavior
+
+var statisticsDecorator = new StatisticsDecorator(cloudMailService);
+statisticsDecorator.SendMail($"Hi there via {nameof(StatisticsDecorator)} wrapper");
+
+
+
+var messageDatabaseDecorator = new MessageDatabaseDecorator(onPremiseMailService);
+var attachCatPngDecorator = new AttachCatPngDecorator(messageDatabaseDecorator, "cat.png");
+attachCatPngDecorator.SendMail($"Hello there via {nameof(MessageDatabaseDecorator)} wrapper, message 1");
+attachCatPngDecorator.SendMail($"Hello there via {nameof(MessageDatabaseDecorator)} wrapper, message 2");
+messageDatabaseDecorator.SendMail($"hello there via {nameof(messageDatabaseDecorator)} with no cat png");
+foreach (var message in messageDatabaseDecorator.SentMessages)
+{
+       Console.WriteLine($"Logged message: {message}");
+}
+
+Console.ReadKey();
