b4d1ef9fbc4b1df489e69743744570937f30d5ae
KennyLevu <kennylevu1@gmail.com>
2025-10-05 22:05:40 -0700

    add state pattern

--- Starter files/Gang of Four Patterns/State/Implementation.cs
+++ Starter files/Gang of Four Patterns/State/Implementation.cs
@@ -1,3 +1,85 @@
 ﻿namespace State
 {
+    // state
+    public abstract class BankAccountState
+    {
+        public BankAccount BankAccount { get; protected set; } = null!;
+        public decimal Balance { get; protected set; }
+        public abstract void Deposit(decimal amount);
+        public abstract void Widthdraw(decimal amount);
+    }
+
+    // concrete state
+    public class RegularState : BankAccountState
+    {
+        public RegularState(decimal balance, BankAccount bankAccount)
+        {
+            Balance = balance;
+            BankAccount = bankAccount;
+        }
+
+        public override void Deposit(decimal amount)
+        {
+            Console.WriteLine($"In {GetType()}, despoiting {amount}");
+            Balance += amount;
+        }
+        public override void Widthdraw(decimal amount)
+        {
+            Console.WriteLine($"In {GetType()}, withdrawing {amount} from {Balance}");
+            Balance -= amount;
+            if (Balance < 0)
+            {
+                BankAccount.BankAccountState = new OverdrawnState(Balance, BankAccount);
+            }
+        }
+    }
+
+    // concrete state
+    public class OverdrawnState : BankAccountState
+    {
+        public OverdrawnState(decimal balance, BankAccount bankAccount)
+        {
+            Balance = balance;
+            BankAccount = bankAccount;
+        }
+
+        public override void Deposit(decimal amount)
+        {
+            Console.WriteLine($"In {GetType()}, despoiting {amount}");
+            Balance += amount;
+            if (Balance >= 0)
+            {
+                BankAccount.BankAccountState = new RegularState(Balance, BankAccount);
+            }
+        }
+        public override void Widthdraw(decimal amount)
+        {
+            Console.WriteLine($"In {GetType()}, widthdrawing from an overdrawn account is not allowed.");
+        }
+    }
+
+
+    // context
+
+    public class BankAccount
+    { 
+        public BankAccountState BankAccountState { get; set; }
+
+        public decimal Balance => BankAccountState.Balance;
+
+        public BankAccount()
+        {
+            BankAccountState = new RegularState(200, this);
+        }
+
+        public void Deposit(decimal amount)
+        {
+            BankAccountState.Deposit(amount);
+        }
+
+        public void Widthdraw(decimal amount)
+        {
+            BankAccountState.Widthdraw(amount);
+        }
+    }
 }
--- Starter files/Gang of Four Patterns/State/Program.cs
+++ Starter files/Gang of Four Patterns/State/Program.cs
@@ -1,1 +1,11 @@
-﻿Console.Title = "State";
+﻿using State;
+
+Console.Title = "State";
+
+BankAccount bankAccount = new();
+bankAccount.Deposit(100);
+bankAccount.Widthdraw(50);
+bankAccount.Widthdraw(500);
+bankAccount.Widthdraw(50);
+bankAccount.Deposit(300);
+bankAccount.Widthdraw(50);
