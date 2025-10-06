841e022975ce229344e763c67848f2700640c722
KennyLevu <kennylevu1@gmail.com>
2025-10-05 17:53:54 -0700

    implement factory

--- Starter files/Gang of Four Patterns/FactoryMethod/Implementation.cs
+++ Starter files/Gang of Four Patterns/FactoryMethod/Implementation.cs
@@ -1,4 +1,83 @@
-﻿namespace FactoryMethod
+﻿using System.ComponentModel;
+
+namespace FactoryMethod
 {
-   
+    public abstract class DiscountService
+    {
+        public abstract int DiscountPercentage { get; }
+        public override string ToString() => GetType().Name;
+    }
+
+    public class CountryDiscountService : DiscountService
+    {
+        private readonly string _countryIdentifier;
+
+        public CountryDiscountService(string countryIdentifier)
+        {
+            _countryIdentifier = countryIdentifier;
+        }
+
+        public override int DiscountPercentage
+        {
+            get
+            {
+                switch (_countryIdentifier)
+                {
+                    case "BE":
+                        return 20;
+                    default:
+                        return 10;
+                }
+            }
+        }
+    }
+
+    public class CodeDiscountService : DiscountService
+    {
+        private readonly Guid _code;
+
+        public CodeDiscountService(Guid code)
+        {
+            _code = code;
+        }
+
+        public override int DiscountPercentage
+        {
+            get => 15;
+        }
+    }
+
+    // creator
+    public abstract class  DiscountFactory
+    {
+        public abstract DiscountService CreateDiscountService();
+    }
+    // concrete creator
+    public class CountryDiscountFactory : DiscountFactory
+    {
+        private readonly string _countryIdentifier;
+        public CountryDiscountFactory(string countryIdentifier)
+        {
+            _countryIdentifier = countryIdentifier;
+        }
+
+        public override DiscountService CreateDiscountService()
+        {
+            return new CountryDiscountService(_countryIdentifier);
+        }
+    }
+
+    public class CodeDiscountFactory : DiscountFactory
+    {
+        private readonly Guid _code;
+        public CodeDiscountFactory(Guid code)
+        {
+            _code = code;
+        }
+
+        public override DiscountService CreateDiscountService()
+        {
+            return new CodeDiscountService(_code);
+        }
+    }
 }
