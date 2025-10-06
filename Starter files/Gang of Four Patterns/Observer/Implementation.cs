127c32895a6499df2a7de2f401900af8c3584725
KennyLevu <kennylevu1@gmail.com>
2025-10-05 22:43:47 -0700

    implement observer pattern

--- Starter files/Gang of Four Patterns/Observer/Implementation.cs
+++ Starter files/Gang of Four Patterns/Observer/Implementation.cs
@@ -1,3 +1,81 @@
 ﻿namespace Observer
 {
+
+
+    // notification
+    public class TicketChange
+    {
+        public int Amount { get; private set; }
+        public int ArtistId { get; private set; }
+
+        public TicketChange(int artistId, int amount)
+        {
+            ArtistId = artistId;
+            Amount = amount;
+        }
+    }
+
+
+    // subject
+    public abstract class TicketChangeNotifier
+    {
+        private List<ITicketChangeListener> _observers = new();
+
+        public void AddObserver(ITicketChangeListener observer)
+        {
+            _observers.Add(observer);
+        }
+
+        public void RemoveObserver(ITicketChangeListener observer)
+        {
+            _observers.Remove(observer);
+        }
+
+        public void Notify(TicketChange ticketChange)
+        {
+            foreach (var observer in _observers)
+            {
+                observer.ReceiveTicketChangeNotification(ticketChange);
+            }
+        }
+    }
+
+    // observer
+    public interface  ITicketChangeListener
+    {
+        void ReceiveTicketChangeNotification(TicketChange ticketChange);
+    }
+
+    /// concrete subject
+    public class OrderService : TicketChangeNotifier
+    {
+        public void CompleteTicketSale(int artistId, int amount)
+        {
+            Console.WriteLine($"{nameof(OrderService)}: is changing its state.");
+            Console.WriteLine($"{nameof(OrderService)}: Notifying observers...");
+            Notify(new TicketChange(artistId, amount));
+        }
+    }
+
+    /// concrete observer
+    /// 
+    public class TicketResellerService : ITicketChangeListener
+    {
+        public void ReceiveTicketChangeNotification(TicketChange ticketChange)
+        {
+            Console.WriteLine($"{nameof(TicketResellerService)}: Reacted to the event. ArtistId: {ticketChange.ArtistId}, Amount: {ticketChange.Amount}");
+        }
+
+    }
+
+    /// concrete observer
+    /// 
+    public class TicketStockService : ITicketChangeListener
+    {
+        public void ReceiveTicketChangeNotification(TicketChange ticketChange)
+        {
+            Console.WriteLine($"{nameof(TicketStockService)}: Reacted to the event. ArtistId: {ticketChange.ArtistId}, Amount: {ticketChange.Amount}");
+        }
+
+    }
 }
--- Starter files/Gang of Four Patterns/Observer/Program.cs
+++ Starter files/Gang of Four Patterns/Observer/Program.cs
@@ -1,1 +1,20 @@
-﻿Console.Title = "Observer";
+﻿using Observer;
+
+Console.Title = "Observer";
+
+
+
+TicketStockService ticketStockService = new();
+TicketResellerService ticketResellerService = new();
+OrderService orderService = new();
+
+
+orderService.AddObserver(ticketStockService);
+orderService.CompleteTicketSale(1, 1000);
+orderService.AddObserver(ticketResellerService);
+orderService.CompleteTicketSale(1, 2000);
+orderService.RemoveObserver(ticketStockService);
+orderService.CompleteTicketSale(2, 3000);
+
+
+Console.ReadKey();
